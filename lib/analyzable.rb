cd8b3fcd78e186fd733920b6f16badfe5a9a224b
Mike Hewitson <mike@hewitson.co.za>
2016-02-23 06:51:07 +0000

    add and test create-report

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,5 @@
+require 'colorizr'
+
 module Analyzable
   def average_price(objects)
     (objects.map { |o| o.price.to_f }.inject(&:+) / objects.size).round(2)
@@ -25,5 +27,18 @@ module Analyzable
       end
     end
     results
+  end
+
+  def print_report(objects)
+    report = "Average Price: $ #{average_price(objects)} \n"
+    report << 'Inventory by ' + 'Brand'.red + ":\n"
+    count_by_brand(objects).each_pair do |k, v|
+      report << '  - ' + k.red + ': ' + v.to_s.blue + "\n"
+    end
+    report << 'Inventory by ' + 'Name'.red + ":\n"
+    count_by_name(objects).each_pair do |k, v|
+      report << '  - ' + k.red + ': ' + v.to_s.blue + "\n"
+    end
+    report
   end
 end
